3a6d541311f7c2b975978f818faf076d5c41cf9a
Александр <109337223+by-Egorov@users.noreply.github.com>
2022-11-18 08:05:41 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from 'react'
-import Balance from './components/Balance/Balance'
-import Header from './components/Header/Header'
-import Operation from './components/Operation/Operation'
-import Story from './components/Story/ Story'
+import React, { useState } from 'react'
+import Wallet from './components/Wallet'
+
 
 function App() {
+
+	const [todo, setTodo] = useState([
+		{
+			id: 1,
+			title: 'получил зарплату',
+			num: 30000,
+			status: true
+		},
+		{
+			id: 2,
+			title: 'оплатил интернет',
+			num: 700,
+			status: false
+		},
+		{
+			id: 3,
+			title: 'у Арсена',
+			num: 2500,
+			status: true
+		},
+	])
   return (
-    <div className="wrapper">
-		<Header/>
-		<Balance/>
-		<Story/>
-		<Operation />
+    <div className="App">
+		<Wallet todo={todo} setTodo={setTodo}/>
     </div>
   )
 }
