396cc366802a7dcac4f88bd90c5caf8fcd34924b
Александр <109337223+by-Egorov@users.noreply.github.com>
2022-11-18 14:23:35 +0300

    Тест2.2

--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -86,7 +86,7 @@ const Wallet = ({ todo, setTodo }) => {
 						</button>
 						<button className='operation__buttons--expenses'
                                                         
-count - numberValue
+
                                                         >
 							Расход
 						</button>
