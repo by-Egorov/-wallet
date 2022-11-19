20f726ad9c7b65f6cebc2130a97827b4a18055cb
Александр <109337223+by-Egorov@users.noreply.github.com>
2022-11-20 00:53:32 +0300

    добавление записей по расходам и доходам + класс записи

--- src/App.js
+++ src/App.js
@@ -2,28 +2,10 @@ import React, { useState } from 'react'
 import Wallet from './components/Wallet'
 
 
+
 function App() {
 
-	const [todo, setTodo] = useState([
-		{
-			id: 1,
-			title: 'получил зарплату',
-			num: 30000,
-			status: true
-		},
-		{
-			id: 2,
-			title: 'оплатил интернет',
-			num: 700,
-			status: false
-		},
-		{
-			id: 3,
-			title: 'у Арсена',
-			num: 2500,
-			status: true
-		},
-	])
+	const [todo, setTodo] = useState([])
   return (
     <div className="App">
 		<Wallet todo={todo} setTodo={setTodo}/>
--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -1,21 +1,46 @@
 import React, { useState } from 'react'
 
+
 const Wallet = ({ todo, setTodo }) => {
-	
 	const [textValue, setTextValue] = useState('')
 	const [numberValue, setNumberValue] = useState('')
-        const money = '₽'
-const [count, setCount] = useState(count = Number(count) + Number(numberValue))
-        
+	const money = '₽'
+	const [countplus, setCountplus] = useState(0)
+	const [countminus, setCountminus] = useState(0)
 
 	function addTodo() {
-		setTodo( [...todo, {
-			id: 4,
-			title: textValue,
-                        num: numberValue,
-			status: false
-		}])
+		
+		setTodo([
+			...todo,
+			{
+				id: null,
+				title: textValue,
+				num: numberValue,
+				class: 'receivedBy',
+				status: '+',
+			},
+		])
+		setTextValue('')
+		setNumberValue('')
 	}
+
+	function addTodoR() {
+		
+		setTodo([
+			...todo,
+			{
+				id: null,
+				title: textValue,
+				num: numberValue,
+				class: 'spentOn',
+				status: '-',
+			},
+		])
+		setTextValue('')
+		setNumberValue('')
+	}
+
+	
 
 	return (
 		<div className='wrapper'>
@@ -27,14 +52,16 @@ const [count, setCount] = useState(count = Number(count) + Number(numberValue))
 				<div className='balance__all'>
 					<div className='balance__all-title'>Баланс</div>
 					<div className='balance__all-meaning'>
-						<span className='balance__all-meaning--current'>{count} {money}</span>
+						<span className='balance__all-meaning--current'>
+							{countplus} {money}
+						</span>
 					</div>
 				</div>
 				<div className='balance__income'>
 					<div className='balance__income-title'>Доходы</div>
 					<div className='balance__income-meaning'>
 						<span className='balance__income-meaning--current'>
-							+ {Number(numberValue) + Number(numberValue)} {money}
+							+ {countplus} {money}
 						</span>
 					</div>
 				</div>
@@ -42,7 +69,7 @@ const [count, setCount] = useState(count = Number(count) + Number(numberValue))
 					<div className='balance__expenses-title'>Расходы</div>
 					<div className='balance__expenses-meaning'>
 						<span className='balance__expenses-meaning--current'>
-							-{Number(numberValue) - Number(numberValue)} {money}
+							- {countminus} {money}
 						</span>
 					</div>
 				</div>
@@ -51,9 +78,9 @@ const [count, setCount] = useState(count = Number(count) + Number(numberValue))
 				<div className='story__title'>История расходов</div>
 				<ul className='story__list'>
 					{todo.map((item) => (
-						<li key={item.id} className='story__item-income receivedBy'>
+						<li key={item.id} className={'story__item-income ' + (item.class)}>
 							<span className='story__item-title'>{item.title}</span>
-							<span className='income__number'>+{item.num} ₽</span>
+							<span className='income__number'>{item.status} {item.num} ₽</span>
 						</li>
 					))}
 				</ul>
@@ -62,33 +89,38 @@ const [count, setCount] = useState(count = Number(count) + Number(numberValue))
 				<div className='operation__title'>
 					<p>Новая запись</p>
 				</div>
-				<div className ='operation__form'>
+				<div className='operation__form'>
 					<input
-						className ='operation__form--text'
-						type ='text'
+						className='operation__form--text'
+						type='text'
 						placeholder='Введите наименование операции'
-						value = {textValue}
-						onChange = {(e) => setTextValue(e.target.value)}
+						value={textValue}
+						onChange={(e) => setTextValue(e.target.value)}
 					/>
 					<input
 						className='operation__form--num'
 						type='text'
 						placeholder='Введите сумму'
 						value={numberValue}
-						onChange = {(e) => setNumberValue(e.target.value)}
+						onChange={(e) => setNumberValue(e.target.value)}
 					/>
 					<div className='operation__buttons'>
 						<button
 							className='operation__buttons--income'
-							
-                                                       
+							onClick={() => {
+								setCountplus(countplus + Number(numberValue))
+								addTodo()
+							}}
 						>
 							Доход
 						</button>
-						<button className='operation__buttons--expenses'
-                                                        
-
-                                                        >
+						<button
+							className='operation__buttons--expenses'
+							onClick={() => {
+								setCountminus(countminus + Number(numberValue))
+								addTodoR()
+							}}
+						>
 							Расход
 						</button>
 					</div>
