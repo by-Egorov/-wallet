21c52143bfd94b054c1945bfdc2f4d0171acdaef
Александр <109337223+by-Egorov@users.noreply.github.com>
2022-11-22 23:54:46 +0300

    Добавил tooltip с датой и проверку на удаление

--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from 'react'
+import Tippy from '@tippy.js/react'
 import uuid from 'react-uuid'
+import 'tippy.js/dist/tippy.css'
 
 const Wallet = ({ todo, setTodo }) => {
 	const [textValue, setTextValue] = useState('')
 	const [numberValue, setNumberValue] = useState('')
 	const money = '₽'
-	const [date, setDate] = useState(JSON.parse(localStorage.getItem('date')) || ''
+	const [date, setDate] = useState(
+		JSON.parse(localStorage.getItem('date')) || ''
 	)
 
 	useEffect(() => {
 		localStorage.setItem('date', JSON.stringify(date))
 	}, [date])
-	
+
 	const [countplus, setCountplus] = useState(
 		JSON.parse(localStorage.getItem('countplus')) || 0
 	)
@@ -40,6 +43,7 @@ const Wallet = ({ todo, setTodo }) => {
 			{
 				id: uuid(),
 				title: textValue,
+				data: new Date().toLocaleDateString(),
 				num: numberValue,
 				class: 'receivedBy',
 			},
@@ -56,6 +60,7 @@ const Wallet = ({ todo, setTodo }) => {
 			{
 				id: uuid(),
 				title: textValue,
+				data: new Date().toLocaleDateString(),
 				num: numberValue,
 				class: 'spentOn',
 			},
@@ -66,36 +71,41 @@ const Wallet = ({ todo, setTodo }) => {
 		setNumberValue('')
 	}
 	function clearHistory() {
-		setTodo(
-			[]
-		)
+		if (window.confirm('You want to delette?')) {
+			setTodo([])
+		}
+		return
 	}
 	function clearCalc() {
-		setBalance(0)
-		setCountplus(0)
-		setCountminus(0)
-		setDate(
-			new Date().toLocaleString()
-		)
+		if (window.confirm('You want to delette?')) {
+			setBalance(0)
+			setCountplus(0)
+			setCountminus(0)
+			setDate(new Date().toLocaleString())
+		}
+		return
 	}
 	
-
 
 	return (
 		<div className='wrapper'>
 			<div className='header'>
 				<div className='header__title'>Wallet</div>
-				<div className="date">{date}</div>
-				<div className="clearHistory">
-						<button onClick={() => {clearCalc()}}>
-						Новая запись
-						<i class="fa fa-refresh" aria-hidden="true"></i>
-						</button>
-					</div>
+				<div className='date'>{date}</div>
+				<div className='clearHistory'>
+					<button
+						onClick={() => {
+							clearCalc()
+						}}
+					>
+						new entry
+						<i class='fa fa-refresh' aria-hidden='true'></i>
+					</button>
+				</div>
 			</div>
 			<div className='balance'>
 				<div className='balance__all'>
-					<div className='balance__all-title'>Баланс</div>
+					<div className='balance__all-title'>Balance</div>
 					<div className='balance__all-meaning'>
 						<span className='balance__all-meaning--current'>
 							{balance} {money}
@@ -103,7 +113,7 @@ const Wallet = ({ todo, setTodo }) => {
 					</div>
 				</div>
 				<div className='balance__income'>
-					<div className='balance__income-title'>Доходы</div>
+					<div className='balance__income-title'>Income</div>
 					<div className='balance__income-meaning'>
 						<span className='balance__income-meaning--current'>
 							+ {countplus} {money}
@@ -111,7 +121,7 @@ const Wallet = ({ todo, setTodo }) => {
 					</div>
 				</div>
 				<div className='balance__expenses'>
-					<div className='balance__expenses-title'>Расходы</div>
+					<div className='balance__expenses-title'>Expenses</div>
 					<div className='balance__expenses-meaning'>
 						<span className='balance__expenses-meaning--current'>
 							- {countminus} {money}
@@ -122,42 +132,50 @@ const Wallet = ({ todo, setTodo }) => {
 			<div className='story'>
 				<div className='story__title'>
 					{/* <div className="title">История</div> */}
-					<div className="clearHistory">
-						<button onClick={() => {clearHistory()}}>
-						Новая история
-						<i class="fa fa-refresh" aria-hidden="true"></i>
+					<div className='clearHistory'>
+						<button
+							onClick={() => {
+								clearHistory()
+							}}
+						>
+							new story
+							<i class='fa fa-refresh' aria-hidden='true'></i>
 						</button>
 					</div>
-					</div>
-					
+				</div>
+
 				<ul className='story__list'>
 					{todo.map((item) => (
-						<li
-							key={item.id}
-							className={'story__item-income ' + item.class}
-						>
-							<span className='story__item-title'>{item.title}</span>
-							<span className='income__number'>{item.num} ₽</span>
-						</li>
+						<Tippy content={item.data}>
+							<li
+								key={item.id}
+								className={'story__item-income ' + item.class}
+							>
+								<span className='story__item-title'>{item.title}</span>
+								<span className='income__number'>
+									{item.num} {money}
+								</span>
+							</li>
+						</Tippy>
 					))}
 				</ul>
 			</div>
 			<div className='operation'>
 				<div className='operation__title'>
-					<p>Новая запись</p>
+					<p>New entry</p>
 				</div>
 				<div className='operation__form'>
 					<input
 						className='operation__form--text'
 						type='text'
-						placeholder='Введите наименование операции'
+						placeholder='Enter a description'
 						value={textValue}
 						onChange={(e) => setTextValue(e.target.value)}
 					/>
 					<input
 						className='operation__form--num'
 						type='text'
-						placeholder='Введите сумму'
+						placeholder='Enter amount'
 						value={numberValue}
 						onChange={(e) => setNumberValue(e.target.value)}
 					/>
@@ -168,7 +186,7 @@ const Wallet = ({ todo, setTodo }) => {
 								addTodo()
 							}}
 						>
-							Доход
+							income
 						</button>
 						<button
 							className='operation__buttons--expenses'
@@ -176,7 +194,7 @@ const Wallet = ({ todo, setTodo }) => {
 								addTodoR()
 							}}
 						>
-							Расход
+							expenses
 						</button>
 					</div>
 				</div>
